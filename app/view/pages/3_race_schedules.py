0b1a7282e85ff2ae56ee7f798a8ac036fe91c8da
takarotoooooo <takarot.0503@gmail.com>
2023-06-05 22:54:09 +0900

    Refactor schedule page

--- app/view/pages/3_race_schedules.py
+++ app/view/pages/3_race_schedules.py
@@ -1,34 +1,37 @@
 import streamlit as st
 import fastf1
-import pandas
+import pandas as pd
 
-st.title('Race schedules')
+import sys
+from pathlib import Path
+if str(Path().resolve()) not in sys.path:
+    sys.path.append(str(Path().resolve()))
+import module.fastf1 as f1
 
-if 'year' not in st.session_state:
+
+def init_session():
     query_params = st.experimental_get_query_params()
-    if 'year' in query_params:
-        st.session_state.year = int(query_params['year'][0])
-    else:
-        st.session_state.year = 2020
 
-# Add a selectbox to the sidebar:
-selected_year = st.sidebar.selectbox(
-    'Year',
-    (2020, 2021, 2022, 2023),
-    key='year'
-)
+    if 'year' not in st.session_state:
+        if 'year' in query_params:
+            st.session_state.year = int(query_params['year'][0])
+        else:
+            st.session_state.year = f1.available_years()[0]
 
-st.experimental_set_query_params(
-    year=selected_year
-)
 
-if selected_year:
-    data_load_state = st.text('Loading data...')
-    event_schedule = fastf1.get_event_schedule(selected_year)
-    event_schedule_pd = pandas.DataFrame(event_schedule)
-    data_load_state.text('')
+def set_query_string():
+    st.experimental_set_query_params(
+        year=st.session_state.year
+    )
 
-    st.dataframe(event_schedule_pd)
+
+def render():
+    st.title(f"{st.session_state.year} Schedule")
+
+    with st.spinner('Loading data...'):
+        event_schedule = fastf1.get_event_schedule(st.session_state.year)
+        event_schedule_pd = pd.DataFrame(event_schedule)
+
     for i, record in enumerate(event_schedule_pd.to_dict('records')):
         try:
             event = event_schedule.get_event_by_round(record['RoundNumber'])
@@ -38,12 +41,20 @@ if selected_year:
         race_session = event.get_race()
         st.markdown(
             f"""
-            - Round : <a href='/race?year={ selected_year }&round={ record['RoundNumber'] }'>{ record['RoundNumber'] }</a>
+            - Round : <a href='/race?year={ st.session_state.year }&round={ record['RoundNumber'] }'>{ record['RoundNumber'] }</a>
             - Name : {record['OfficialEventName']}
             - Country : {record['Country']}
             - Location : {record['Location']}
             - Date : {race_session.date}
             """,
             unsafe_allow_html=True)
-else:
-    st.error("Please select year.")
+
+
+def main():
+    init_session()
+    set_query_string()
+    render()
+
+
+if __name__ == "__main__":
+    main()

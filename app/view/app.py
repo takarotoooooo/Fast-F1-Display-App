ff64be6b5ca968c118bf7a68deb4ad59d0807e02
takarotoooooo <takarot.0503@gmail.com>
2023-06-11 22:30:52 +0900

    Move to sample

--- app/view/app.py
+++ app/view/app.py
@@ -1,12 +1,3 @@
 import streamlit as st
-import fastf1
-import pandas
 
-st.write('Hello')
-
-event_schedule = fastf1.get_event_schedule(2022)
-pd = pandas.DataFrame(event_schedule)
-st.write(pd)
-
-event = fastf1.get_event(2022, 1)
-st.write(event.get_race())
+st.title('FastF1 display APP')

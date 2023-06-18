08e4284c883e4e728f603d1ee28970588b744092
takarotoooooo <takarot.0503@gmail.com>
2023-06-18 22:47:11 +0900

    For checking deploy

--- app/view/top.py
+++ app/view/top.py
@@ -3,6 +3,11 @@ import sys
 from pathlib import Path
 if str(Path().resolve()) not in sys.path:
     sys.path.append(str(Path().resolve()))
+
+st.write(Path().resolve())
+st.write(sys.path)
+st.write(st.__version__)
+
 import module.fastf1 as f1
 import importlib
 importlib.reload(f1)
